1cc7ba5b085a3923b7d3abae12f3b34e80c38b81
F415alarrahman <f415alarr@gmail.com>
2024-11-06 09:40:56 +0700

    Add homepage layout and initial design

--- lib/utils/assets_path.dart
+++ lib/utils/assets_path.dart
@@ -3,6 +3,9 @@ const String icon = "assets/icons";
 
 class ImageAssets {
   static const String logo = "$icon/logo-circle.png";
+  static const String logoChatbot = "$icon/AGROLYN (8).png";
+  static const String logoJagung = "$icon/AGROLYN (6).png";
+  static const String logoPadi = "$icon/AGROLYN (7).png";
   static const String logoGoogle = "$icon/google_PNG19635.png";
   static const String background = "$root/bg.png";
   static const String onboarding1 = "$root/foods.png";
@@ -13,4 +16,12 @@ class ImageAssets {
   static const String petani = "$root/AGROLYN (3).png";
   static const String user = "$root/AGROLYN (4).png";
   static const String user2 = "$root/AGROLYN (5).png";
+  static const String banner1 =
+      "$root/hijau putih minimalis makanan pokok global padi instagram post (1.920 x 1.080 piksel) (1) 1.png";
+  static const String banner2 = "$root/banner-community.png";
+  static const String banner3 =
+      "$root/DALL·E 2024-11-04 12.49.39 - A 2D illustration of agriculture depicting crop failure due to plant disease in Indonesia. The scene shows a landscape of withered crops, some leaves  1.png";
+  static const String banner4 =
+      "$root/DALL·E 2024-11-04 12.51.07 - A 2D illustration of agriculture depicting rice and corn crop failure due to plant disease in Indonesia. The scene shows a landscape of withered rice  1.png";
+  static const String jagung = "$root/image (1).png";
 }
