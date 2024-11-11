81dcf4bfa2e89f0ccfc197179a74df136962f1e1
dodichakill <dodiherlangga91@gmail.com>
2024-11-11 20:43:30 +0700

    temp

--- lib/views/Profile/information_screen.dart
+++ lib/views/Profile/information_screen.dart
@@ -5,104 +5,130 @@ import 'package:agrolyn/utils/assets_path.dart';
 import 'package:agrolyn/widgets/text_input.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class InformationScreen extends StatelessWidget {
+class InformationScreen extends StatefulWidget {
   const InformationScreen({super.key});
 
   @override
+  State<InformationScreen> createState() => _InformationScreenState();
+}
+
+class _InformationScreenState extends State<InformationScreen> {
+  final keyFrom = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController phoneNumberController = TextEditingController();
+  final TextEditingController addressController = TextEditingController();
+  String? name;
+  @override
+  void initState() {
+    super.initState();
+    loadUserData();
+  }
+
+  Future<void> loadUserData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      name = prefs.getString('name');
+      nameController.text = prefs.getString('name') ?? '';
+      emailController.text = prefs.getString('email') ?? '';
+      phoneNumberController.text = prefs.getString('phone_number') ?? '';
+      addressController.text = prefs.getString('alamat') ?? '';
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => InformationNotifier(context: context),
-      child: Consumer<InformationNotifier>(
-          builder: (context, value, child) => Scaffold(
-                  body: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(Icons.arrow_back)),
+                  const SizedBox(width: 12),
+                  const Text(
+                    "Informasi Akun",
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 32),
+              InkWell(
+                onTap: () async {
+                  // imageProvider.pickImage(ImageSource.gallery);
+                  // ApiService().getRequest("/articles")
+                },
+                child: Image.asset(
+                  ImageAssets.logo,
+                  height: 120,
+                  width: 120,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  key: keyFrom,
                   child: Column(
                     children: [
-                      Row(
-                        children: [
-                          IconButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                              },
-                              icon: const Icon(Icons.arrow_back)),
-                          const SizedBox(width: 12),
-                          const Text(
-                            "Informasi Akun",
-                            style: TextStyle(
-                                fontWeight: FontWeight.w600, fontSize: 20),
-                          ),
-                        ],
-                      ),
                       const SizedBox(height: 32),
-                      InkWell(
-                        onTap: () async {
-                          // imageProvider.pickImage(ImageSource.gallery);
-                          // ApiService().getRequest("/articles")
-                        },
-                        child: Image.asset(
-                          ImageAssets.logo,
-                          height: 120,
-                          width: 120,
-                        ),
+                      TextInput(
+                        name: "Nama",
+                        defaultValue: nameController.text,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Form(
-                          key: value.keyform,
-                          child: Column(
-                            children: [
-                              const SizedBox(height: 32),
-                              const TextInput(
-                                name: "Nama",
-                                defaultValue: "Daniel Pratama",
-                              ),
-                              const SizedBox(height: 12),
-                              const TextInput(
-                                name: "Email",
-                                isDisabled: true,
-                                defaultValue: "johndoe123@gmail.com",
-                              ),
-                              const SizedBox(height: 12),
-                              const TextInput(
-                                name: "Nomor Hp",
-                                defaultValue: "08123456789",
-                              ),
-                              const SizedBox(height: 12),
-                              const TextInput(
-                                  name: "Alamat",
-                                  defaultValue: "Kota Tegal, Jawa Tengah"),
-                              const SizedBox(height: 12),
-                              const Divider(
-                                color: Color.fromARGB(105, 166, 243, 178),
-                              ),
-                              const SizedBox(height: 12),
-                              SizedBox(
-                                width: double.infinity,
-                                height: 56,
-                                child: ElevatedButton(
-                                  onPressed: () {},
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: MyColors.primaryColorDark,
-                                  ),
-                                  child: const Text(
-                                    'Simpan Perubahan',
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                ),
-                              ),
-                            ],
+                      const SizedBox(height: 12),
+                      TextInput(
+                        name: "Email",
+                        isDisabled: true,
+                        defaultValue: emailController.text,
+                      ),
+                      const SizedBox(height: 12),
+                      TextInput(
+                        name: "Nomor Hp",
+                        defaultValue: phoneNumberController.text,
+                      ),
+                      const SizedBox(height: 12),
+                      TextInput(
+                          name: "Alamat", defaultValue: addressController.text),
+                      const SizedBox(height: 12),
+                      const Divider(
+                        color: Color.fromARGB(105, 166, 243, 178),
+                      ),
+                      const SizedBox(height: 12),
+                      SizedBox(
+                        width: double.infinity,
+                        height: 56,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: MyColors.primaryColorDark,
+                          ),
+                          child: const Text(
+                            'Simpan Perubahan',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
                           ),
                         ),
-                      )
+                      ),
                     ],
                   ),
                 ),
-              ))),
+              ),
+              Text("$name"),
+              Text(emailController.text),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
